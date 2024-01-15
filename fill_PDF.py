fc6ea9e7d740f44f2a59b49ba8754f6d5ee351f2
Anthony Yan <yanthony@berkeley.edu>
2024-01-14 23:46:56 -0800

    Updated fill PDF function to be similar to anthony_test.py where the code makes a copy of blank PDF then append to the copied PDF so hopefully the font and location of the form does not change over time.

--- fill_PDF.py
+++ fill_PDF.py
@@ -1,9 +1,15 @@
 from pypdf import PdfReader, PdfWriter
 from datetime import datetime
+import os
 
 def fill_infoSheet (dict_data, contract_number, employer_hkid):
     try:
-        reader = PdfReader('blank_PDF/blank_infoSheet.pdf')
+        # Make a copy of original file then append the copied PDF
+        blank_infoSheet_source_path = 'blank_PDF/blank_infoSheet.pdf'
+        blank_infoSheet_to_fill_path = 'blank_PDF/blank_infoSheet_to_fill.pdf'
+        copy_and_paste_file(blank_infoSheet_source_path, blank_infoSheet_to_fill_path)
+
+        reader = PdfReader(blank_infoSheet_to_fill_path)
         writer = PdfWriter()
 
         writer.append(reader)
@@ -14,13 +20,46 @@ def fill_infoSheet (dict_data, contract_number, employer_hkid):
         with open(employer_hkid + "_" + contract_number + "_infoSheet.pdf", "wb") as output_stream:
             writer.write(output_stream)
 
+        # Remove the copied PDF that undergone above steps
+        os.remove(blank_infoSheet_to_fill_path)
+
         print("infoSheet done")
+    except Exception as e:
+        print(f'Error: {e}')
+
+def fill_transmittalForm (dict_data, contract_number, employer_hkid):
+    try:
+        # Make a copy of original file then append the copied PDF
+        blank_transmittalForm_source_path = 'blank_PDF/blank_transmittalForm.pdf'
+        blank_transmittalForm_to_fill_path = 'blank_PDF/blank_transmittalForm_to_fill.pdf'
+        copy_and_paste_file(blank_transmittalForm_source_path, blank_transmittalForm_to_fill_path)
+
+        reader = PdfReader(blank_transmittalForm_to_fill_path)
+        writer = PdfWriter()
+        
+        writer.append(reader)
+
+        for i in range(len(reader.pages)):
+            writer.update_page_form_field_values(writer.pages[i], dict_data)
+        
+        with open(employer_hkid + "_" + contract_number + "_transmittalForm.pdf", "wb") as output_stream:
+            writer.write(output_stream)
+
+        # Remove the copied PDF that undergone above steps
+        os.remove(blank_transmittalForm_to_fill_path)
+
+        print("Transmittal form done")
     except Exception as e:
         print(f'Error: {e}')
 
 def fill_owwa(dict_data, contract_number, employer_hkid):
     try:
-        reader = PdfReader('blank_PDF/blank_owwa.pdf')
+        # Make a copy of original file then append the copied PDF
+        blank_owwa_source_path = 'blank_PDF/blank_owwa.pdf'
+        blank_owwa_to_fill_path = 'blank_PDF/blank_owwa_to_fill.pdf'
+        copy_and_paste_file(blank_owwa_source_path, blank_owwa_to_fill_path)
+
+        reader = PdfReader(blank_owwa_to_fill_path)
         writer = PdfWriter()
 
         writer.append(reader)
@@ -31,14 +70,22 @@ def fill_owwa(dict_data, contract_number, employer_hkid):
         with open(employer_hkid + "_" + contract_number + "_owwa.pdf", "wb") as output_stream:
             writer.write(output_stream)
 
+
+        # Remove the copied PDF that undergone above steps
+        os.remove(blank_owwa_to_fill_path)
+
         print("OWWA form done")
     except Exception as e:
         print(f'Error: {e}')
 
-    
 def fill_id407(dict_data, contract_number, employer_hkid):
     try:
-        reader = PdfReader('blank_PDF/blank_id407.pdf')
+        # Make a copy of original file then append the copied PDF
+        blank_id407_source_path = 'blank_PDF/blank_id407.pdf'
+        blank_id407_to_fill_path = 'blank_PDF/blank_id407_to_fill.pdf'
+        copy_and_paste_file(blank_id407_source_path, blank_id407_to_fill_path)
+
+        reader = PdfReader(blank_id407_to_fill_path)
         writer = PdfWriter()
 
         writer.append(reader)
@@ -49,14 +96,21 @@ def fill_id407(dict_data, contract_number, employer_hkid):
         with open(employer_hkid + "_" + contract_number + "_id407.pdf", "wb") as output_stream:
             writer.write(output_stream)
 
+        # Remove the copied PDF that undergone above steps
+        os.remove(blank_id407_to_fill_path)
+
         print("id407 done")
     except Exception as e:
         print(f'Error: {e}')
 
-
 def fill_acknowledgement_confirmation_form(dict_data, contract_number, employer_hkid):
     try:
-        reader = PdfReader('blank_PDF/blank_golden_joy_acknowledgement_confirmation.pdf')
+        # Make a copy of original file then append the copied PDF
+        blank_golden_joy_acknowledgement_confirmation_source_path = 'blank_PDF/blank_golden_joy_acknowledgement_confirmation.pdf'
+        blank_golden_joy_acknowledgement_confirmation_to_fill_path = 'blank_PDF/blank_golden_joy_acknowledgement_confirmation_to_fill.pdf'
+        copy_and_paste_file(blank_golden_joy_acknowledgement_confirmation_source_path, blank_golden_joy_acknowledgement_confirmation_to_fill_path)
+
+        reader = PdfReader(blank_golden_joy_acknowledgement_confirmation_to_fill_path)
         writer = PdfWriter()
 
         writer.append(reader)
@@ -67,10 +121,12 @@ def fill_acknowledgement_confirmation_form(dict_data, contract_number, employer_
         with open(employer_hkid + "_" + contract_number + "_acknowledgement_confirmation.pdf", "wb") as output_stream:
             writer.write(output_stream)
 
+        # Remove the copied PDF that undergone above steps
+        os.remove(blank_golden_joy_acknowledgement_confirmation_to_fill_path)
+
         print("acknowledgement and confirmation done")
     except Exception as e:
         print(f'Error: {e}')
-
 
 def split_long_string(input_string, max_lengths):
     # Split the input string into words
@@ -96,6 +152,47 @@ def split_long_string(input_string, max_lengths):
             result_strings.append("")
 
     return result_strings
+
+def copy_and_paste_file(source_path, destination_path):
+    bash_script_content = f'''
+#!/bin/bash
+
+# Declare variable in bash script
+source_path="{source_path}"
+destination_path="{destination_path}"
+
+# Use variable in bash script
+cp -R $source_path $destination_path
+'''
+    # Write the bash script content to a file
+    bash_script_path = 'generate_script.sh'
+    with open(bash_script_path, 'w') as bash_script_file: bash_script_file.write(bash_script_content)
+
+    # Make the Bash script executable
+    os.system(f'chmod +x {bash_script_path}')
+
+    # Run the Bash script
+    os.system(f'./{bash_script_path}')
+
+    # Optionally, you can remove the generated Bash script after execution
+    os.remove(bash_script_path)
+    
+    return None
+
+def calculate_birthday (birthdate):
+    if birthdate != "":
+        # Convert the birthdate string to a datetime object
+        birth_date = datetime.strptime(birthdate, "%Y-%m-%d")
+
+        # Get the current date
+        current_date = datetime.now()
+
+        # Calculate the age
+        age = current_date.year - birth_date.year - ((current_date.month, current_date.day) < (birth_date.month, birth_date.day))
+        
+        return age
+    else:
+        return ""
 
 def calculate_birthday (birthdate):
     if birthdate != "":
