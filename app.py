879fc0975b5e8c43f1a4adfe5656be462069c64f
Anthony Yan <yanthony@berkeley.edu>
2024-10-12 16:44:31 -0700

    move constants into its own python file and import it into app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from fill_PDF import *
 import math
 import traceback
+from CONSTANT import *
 
 # Create the main window
 app = QApplication(sys.argv)
@@ -20,100 +21,6 @@ window.setCentralWidget(main_widget)
 # Layout for the main widget
 main_layout = QVBoxLayout()
 main_widget.setLayout(main_layout)
-
-# Create constant dictionary
-constant_dict = {
-    "hk_agency_name": "JOJO GOOD WELL EMPLOYMENT AGENCY LIMITED",
-    "hk_agency_phone_number": "64656060",
-    "philippine_agency_name": "PLACEWELL INTERNATINOAL SERVICE CORPORATION",
-    "philippine_agency_phone_number": "63285264838",
-    "philippine_agency_code": "MWOHK-2023-170",
-    "witness1_name": "CHOW SUK FAN",
-    "witness2_name": "KATHERINE CHOW"
-}
-# Create dictionaries to store user input
-employer_pane_dict = {
-   # Misc.
-    "contract_category": "NEW",     # drop down set default contract_category to NEW
-    "contract_number": "",
-    "contract_date": "",
-    "owwa_recipt_number": "",
-    "wage": "4990",     # set default wage = 4990
-    "allowance": "1236",    # set default allowance = 1236
-    "other_allowance": "",
-    "paid_vacation_for_renew_contract": "N/A",     # set default to N/A
-    # Employer details
-    "employer_sur_name": "",
-    "employer_first_name": "",
-    "employer_middle_name": "",
-    "employer_birthday": "",
-    # "employer_age": "",
-    "employer_gender": "",
-    "employer_nationality": "CHINESE",  # set default nationality to CHINESE
-    "employer_hkid": "",
-    "employer_passport_number": "",
-    "employer_civil_status": "SINGLE",  # set default civil status to SINGLE
-    "employer_email": "",
-    "employer_hk_phone_number": "",
-    "employer_spouse_sur_name": "",
-    "employer_spouse_first_name": "",
-    "employer_spouse_middle_name": "",
-    "employer_spouse_hkid": "",
-    "employer_spouse_passport_number": "",
-    "employer_address": "",
-    "employer_residential_type": "FLAT",    # set default residential type to FLAT
-    "employer_residential_size": "",
-    "employer_house_type_remarks": "",
-    "employer_expected_baby": "",
-    "employer_child_0_to_5": "",
-    "employer_child_5_to_18": "",
-    "employer_adult": "",
-    # "employer_people_to_be_served": "",
-    "employer_current_worker_number": "",
-    "employer_servant_own_room": "YES",    # set default servant room to YES
-    "employer_servant_own_room_size": "",
-    # "employer_servant_room_shared_with": "",
-    "employer_servant_share_room_with_how_many_children": "",
-    "employer_servant_share_room_with_children_age": "",
-    "employer_servant_room_other_with_remarks": "",
-    "employer_provide_light_and_water_supply": "YES",       # set default to YES
-    "employer_provide_toilet_and_bathing_facilities": "YES",       # set default to YES
-    "employer_provide_bed": "YES",       # set default to YES
-    "employer_provide_blankets_or_quilt": "YES",       # set default to YES
-    "employer_provide_pillows": "YES",       # set default to YES
-    "employer_provide_wardrobe": "YES",       # set default to YES
-    "employer_provide_refrigerator": "NO",       # set default to NO
-    "employer_provide_desk": "NO",       # set default to NO
-    "employer_provide_other_facilities": "",
-    "employer_expect_other_duties": ""
-    }
-helper_pane_dict = {
-    "helper_sur_name": "",
-    "helper_first_name": "",
-    "helper_middle_name": "",
-    "helper_birthday": "",
-    "helper_place_of_birth": "",
-    # "helper_age": "",
-    "helper_gender": "",
-    "helper_civil_status": "SINGLE",    # set default civil status to SINGLE
-    "helper_hkid": "",
-    "helper_passport_number": "",
-    "helper_passport_issue_place": "",
-    "helper_passport_issue_date": "",
-    "helper_passport_expire_date": "",
-    "helper_visa_expire_date": "",
-    "helper_id522_appointment_date": "",
-    "helper_hk_phone_number": "",
-    "helper_philippine_phone_number": "",
-    "helper_address": "",
-    "helper_previous_contract_number": "",
-    "helper_previous_contract_finish_date": "",
-    "helper_philippine_contact_person": "",
-    "helper_philippine_contact_person_relationship": "",
-    "helper_philippine_contact_person_phone_number": "",
-    "helper_philippine_contact_person_birthday": "",
-    "helper_philippine_contact_person_address": "",
-}
 
 # Employer pane with a vertical scroll
 employer_scroll_area = QScrollArea()
@@ -147,7 +54,7 @@ for key, value in employer_pane_dict.items():
         employer_layout.addWidget(text_input)
     elif key == "wage":
         text_input = QLineEdit()
-        text_input.setText("4870")
+        text_input.setText(employer_pane_dict["wage"])
         text_input.textChanged.connect(lambda text, key=key: employer_input_changed(text, key))
         employer_layout.addWidget(label)
         employer_layout.addWidget(text_input)
@@ -645,6 +552,7 @@ def on_submit():
         # Service Agreement
         combined_data["service_agreement_contract_date"] = datetime.now().strftime('%Y-%m-%d')
         combined_data["helper_name_consent"] = combined_data["helper_sur_name"] + " " + combined_data["helper_first_name"] + " " + combined_data["helper_middle_name"]
+        combined_data["commission_fee_hkd"] = str(math.ceil(int(combined_data["wage"]) * 0.1))
         combined_data["fdh_name"] = combined_data["helper_name_consent"]
         combined_data["service_agreement_fdh_signature_date"] = datetime.now().strftime('%Y-%m-%d')
         combined_data["service_agreement_hk_agency_signature_date"] = datetime.now().strftime('%Y-%m-%d')
